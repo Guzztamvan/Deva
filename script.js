776db62854ea3b8b2c4799a58d324f7adb0bdc0b
Guzztamvan <bh569433@gmail.com>
2024-11-28 09:46:37 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function insert(num){
+  document.form.display.value = document.form.display.value + num;
+}
+function equal(){
+  var hasil = document.form.display.value;
+  document.form.display.value = eval(hasil);
+}
+function clean(){
+  document.form.display.value = "";
+}
+function back(){
+  var hasil = document.form.display.value;
+  document.form.display.value = hasil.substring(0,hasil.length-1);
+}
