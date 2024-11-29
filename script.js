c69e0d5b29ec7669fba6fc62d7d2dab69bd581cf
Guzztamvan <bh569433@gmail.com>
2024-11-29 11:09:48 +0700

    Update script.js
    yy

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function insert(num){
     document.form.display.value = eval(hasil);
   }
   function clean(){
-    document.form.display.value = "";
+    document.form.display.value = "done";
   }
   function back(){
     var hasil = document.form.display.value;
