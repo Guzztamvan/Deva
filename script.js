f1370d6450888403facdd1c807826e30c103c5eb
Guzztamvan <bh569433@gmail.com>
2024-11-29 11:06:11 +0700

    Update script.js
    masalah di bagian script sudah selesai

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function insert(num){
-<<<<<<< HEAD
     document.form.display.value = document.form.display.value + num;
   }
   function equal(){
@@ -13,18 +12,3 @@ function insert(num){
     var hasil = document.form.display.value;
     document.form.display.value = hasil.substring(0,hasil.length-1);
   }
-=======
-  document.form.display.value = document.form.display.value + num;
-}
-function equal(){
-  var hasil = document.form.display.value;
-  document.form.display.value = eval(hasil);
-}
-function clean(){
-  document.form.display.value = "sigmamel🤫";
-}
-function back(){
-  var hasil = document.form.display.value;
-  document.form.display.value = hasil.substring(0,hasil.length-1);
-}
->>>>>>> 776db62854ea3b8b2c4799a58d324f7adb0bdc0b
